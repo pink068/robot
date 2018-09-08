01aa06ab4a6e54d381f81ada7afd0fc9ab0de004
David Pink <pink.david.j@gmail.com>
2018-09-08 20:29:55 +0100

    Add files via upload

---
+++ myxmlrpc.cpp
@@ -0,0 +1,39 @@
+#include <sys/types.h>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <iostream>
+#include <time.h>
+#include "flxml.h"
+
+// using namespace XmlRpc;
+//XmlRpcClient c ("localhost",7362);
+
+int flxml::Initialise(void)
+{
+
+    return 0;
+}
+int flxml::ReadString(char * rxbuffer, const char * request)
+{
+
+  return strlen(rxbuffer);
+}
+
+int flxml::ReadCharArray(char * rxbuffer, const char * request)
+{
+
+  return strlen(rxbuffer);
+}
+
+static void flxml:: dieIfFaultOccurred (xmlrpc_env * const envP)
+{
+    if (envP->fault_occurred) {
+        fprintf(stderr, "ERROR: %s (%d)\n",
+                envP->fault_string, envP->fault_code);
+        exit(1);
+    }
+}
+
---
+++ myxmlrpc.h
@@ -0,0 +1,4 @@
+int XmlRpc_ReadString(char * rxbuffer, const char * request);
+int XmlRpc_ReadCharArray(char * rxbuffer, const char * request);
+int XmlRpc_ReadInt(const char * request);
+int XmlRpc_Close(void);
